c324f2746d9a843c38394b907003ef34e1204d14
finlay brydges <100164471+finlay-ab@users.noreply.github.com>
2026-01-30 15:25:47 +0000

    Added Return to Depot after delivery; Refactored Metrics and SpatialManager (name replaced with Id and agent/ drone now UAV)

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import simpy
 import random
 import numpy as np
 import logging
+from entities import UAV
 from environment import SpatialManager, Metrics
 from policies.greedy import GreedyPolicy
 
@@ -11,41 +12,24 @@ SIM_TIME = 200      # total sim seconds
 LAMBDA = 1/12       # poisson rate (aprox 1 job per 12s)
 SAFETY_RADIUS = 2.0 
 MAX_SPEED = 5.0     
+FLEET_SIZE = 4      # number of active UAVs
 
 # set logging
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 log = logging.getLogger("UAS_Sim")
 
-# drone process
-def drone_process(env, name, start, goal, sm, metrics, policy):
-    pos = np.array(start, dtype=float)
-    target = np.array(goal, dtype=float)
-    
-    # while not within 0.5 units of target
-    while np.linalg.norm(target - pos) > 0.5:
-        metrics.record_path(name, pos)
-        
-        # get velocity 
-        vel = policy.get_velocity(name, pos, target, sm)
-        
-        pos += vel * DT
-        sm.update(name, pos)
-        yield env.timeout(DT)
-    
-    metrics.completed_deliveries += 1
-    log.info(f"[{env.now:4.1f}] {name} arrived.")
-
-def job_generator(env, sm, metrics, policy):
+def job_generator(env, job_queue):
     job_count = 0
     while True:
         # poisson inter-arrival time
         yield env.timeout(random.expovariate(LAMBDA))
         job_count += 1
-        name = f"job: {job_count}"
-        log.info(f"[{env.now:4.1f}] {name} created and starting flight.")
-        start = [random.uniform(0, 100), random.uniform(0, 100)]
-        goal = [random.uniform(0, 100), random.uniform(0, 100)]
-        env.process(drone_process(env, name, start, goal, sm, metrics, policy))
+        job = {
+            'id': job_count, 
+            'goal': [random.uniform(0, 100), random.uniform(0, 100)]
+        }
+        log.info(f"[{env.now:4.1f}] Job {job_count} requested.")
+        job_queue.put(job)
 
 def monitor(env, sm, metrics):
     while True:
@@ -58,12 +42,17 @@ if __name__ == "__main__":
     sm = SpatialManager(SAFETY_RADIUS)
     met = Metrics()
     pol = GreedyPolicy(MAX_SPEED)
+    job_queue = simpy.Store(env)
 
-    env.process(job_generator(env, sm, met, pol))
+    # initialize fleet at Depot [10, 10]
+    for i in range(FLEET_SIZE):
+        UAV(env, f"UAV_{i}", [10, 10], sm, met, pol, job_queue, DT)
+
+    env.process(job_generator(env, job_queue))
     env.process(monitor(env, sm, met))
     
     env.run(until=SIM_TIME)
     
     print(f"\nExperiment Results:")
     print(f"Total Deliveries: {met.completed_deliveries}")
-    print(f"Separation Violations: {met.total_violations}")
+    print(f"Total Separation Violations: {met.total_violations}")
