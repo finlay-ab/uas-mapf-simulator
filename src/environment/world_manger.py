b49e54ef40aa3fb177f648653c86019db5aeb2cf
finlay brydges <100164471+finlay-ab@users.noreply.github.com>
2026-04-17 17:25:02 +0000

    Added a world manager that will manage all the airspaces

---
+++ src/environment/world_manger.py
@@ -0,0 +1,44 @@
+import json
+from src.environment.airspace import Airspace
+from src.environment.map import GridMap
+
+class WorldManager:
+    def __init__(self, config, env, policy, job_queue):
+        self.config = config
+        self.env = env
+        self.policy = policy
+        self.job_queue = job_queue
+        self.airspaces = {}
+        self.graph = {}
+        
+        # load airspaces
+        self._load_airspaces()
+        
+        # create graph
+        self._create_graph()
+    
+    def _load_airspaces(self):
+        # read json
+        with open(self.config.world_config, 'r') as f:
+            manifest = json.load(f)
+        
+       # for each airspace in json create airspace object
+        for airspace in manifest['airspaces']:
+            # get config files 
+            airspace_id = airspace['id']
+            config_file = airspace['config_file']
+            map_file = airspace['map_file']
+            fleet_file = airspace['fleet_file']
+            
+            # create airspace and store in dict
+            self.airspaces[airspace_id] = Airspace(airspace_id, config_file, map_file, fleet_file, self.env, self.policy, self.job_queue, self.config)
+    
+    def _create_graph(self):
+        for airspace_id in self.airspaces.keys():
+            self.graph[airspace_id] = [] 
+    
+    def get_airspace(self, airspace_id):
+        return self.airspaces.get(airspace_id, None)
+    
+    def get_all_airspaces(self):
+        return self.airspaces
