a9f7b506cecedb95a0236f22fcbf5c3e79979570
finlay brydges <100164471+finlay-ab@users.noreply.github.com>
2026-05-12 13:12:11 +0000

    Added loading from json and seeds to config

--- src/config.py
+++ src/config.py
@@ -1,4 +1,7 @@
-from dataclasses import dataclass
+import json
+from dataclasses import dataclass, fields
+from enum import Enum
+
 from src.schemas import PolicyType, WrapperType, PathRecoveryStrategy, PathRecoveryAction
 
 @dataclass
@@ -15,6 +18,8 @@ class SimConfig:
     file: str = "src/environment/obstacles.txt"
 
     world_config: str = "config/worlds/default/world_manifest.json"
+
+    waypoint_arrival_threshold: float = 1.5
    
     # job gen rate
     lambda_rate: float = 1/12   # poisson rate (approx 1 job per 12s)
@@ -24,10 +29,13 @@ class SimConfig:
     max_speed: float = 5.0      # max UAV speed [m/s]
     safety_radius: float = 2.5  # collision avoidance radius [m]
 
+    # when false uavs will hover wait when a potential collision could happen
+    allow_predicted_collisions: bool = True
+
     # policy and wrapper
     policy: PolicyType = PolicyType.GREEDY
     wrapper: WrapperType = WrapperType.NONE
-    connectivity: int = 4
+    connectivity: int = 8
 
     # path recovery 
     path_recovery_strategy: PathRecoveryStrategy = PathRecoveryStrategy.RETURN_TO_NEXT_WP
@@ -49,3 +57,42 @@ class SimConfig:
     # out
     log_file: str = "simulation.log"
     csv_file: str = "run_results.csv"
+
+    # random 
+    seed: int | None = None
+
+    # load config from dict/ file
+    @classmethod
+    def from_dict(class_, data):
+        # gets str to enum
+        enum_fields = {
+            "policy": PolicyType,
+            "wrapper": WrapperType,
+            "path_recovery_strategy": PathRecoveryStrategy,
+            "path_recovery_action": PathRecoveryAction,
+        }
+
+        valid_keys = {f.name for f in fields(class_)}
+        converted = {}
+
+        for key, value in data.items():
+            if key not in valid_keys:
+                raise ValueError(f"unknown sim config: {key}")
+
+            # turn enum to key 
+            if key in enum_fields and isinstance(value, str):
+                value = enum_fields[key][value.strip().upper()]
+
+            converted[key] = value
+
+        return class_(**converted)
+
+    @classmethod
+    def from_file(class_, config_file, overrides=None):
+        with open(config_file, 'r') as f:
+            data = json.load(f)
+
+        if overrides:
+            data.update(overrides)
+
+        return class_.from_dict(data)
